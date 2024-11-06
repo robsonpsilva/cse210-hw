387f7c8ecad6d460a5539d178bb778d87cf45e2e
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-06 06:15:51 -0300

    modified:   csharp-prep/Prep1/Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        string firstName;
+        string lastName;
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
