b03187f6902139552903515e82088d381febcc3d
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-06 18:47:01 -0300

    modified:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        int magic;
+        int guess;
+
+        Console.Write("What is the magic number? ");
+        magic = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (magic > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magic < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
