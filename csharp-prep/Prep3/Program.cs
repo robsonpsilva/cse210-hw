b576581f17c1b86621b3a38847b569a747bc4201
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-06 20:07:07 -0300

    modified:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,30 +6,37 @@ class Program
     {
         Console.WriteLine("Hello Prep3 World!");
 
-        int magic;
-        int guess;
+        int magic = 0;
+        int guess = 1;
         bool stopGuessing = false;
         string opt;
+        int count = 0;
 
         Random randomGenerator = new Random();
         magic = randomGenerator.Next(0, 100);
-        while (stopGuessing == false) 
+        while (stopGuessing == false)
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            while (magic != guess) 
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
 
-            if (magic > guess)
-            {
-                Console.WriteLine("Higher");
+                if (magic > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magic < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                count++;
+                
             }
-            else if (magic < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+            Console.WriteLine($"You made {count} attempts until you hit the magic number.");
 
             Console.Write("Would you like to end the game? Enter y for yes or n for no. ");
             opt = Console.ReadLine();
