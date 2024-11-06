94dabf2863f87fcbac7bb3cc812a13b8f1435ab3
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-06 19:15:41 -0300

    modified:   csharp-prep/Prep3/Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-class Program
+class Program 
 {
     static void Main(string[] args)
     {
@@ -8,25 +8,37 @@ class Program
 
         int magic;
         int guess;
+        bool stopGuessing = false;
+        string opt;
 
-        Console.Write("What is the magic number? ");
-        magic = int.Parse(Console.ReadLine());
-
-        Console.Write("What is your guess? ");
-        guess = int.Parse(Console.ReadLine());
-
-        if (magic > guess)
+        Random randomGenerator = new Random();
+        magic = randomGenerator.Next(0, 100);
+        while (stopGuessing == false) 
         {
-            Console.WriteLine("Higher");
-        }
-        else if (magic < guess)
-        {
-            Console.WriteLine("Lower");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
 
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+            Console.Write("Would you like to end the game? Enter y for yes or n for no. ");
+            opt = Console.ReadLine();
+            if (opt == "y")
+            {
+                stopGuessing = true;
+            }
+        }
     }
+
+        
 }
