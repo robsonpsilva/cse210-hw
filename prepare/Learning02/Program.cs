aed4ed9bb0d1e04c7e7bc5f56d679a1901b603c8
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-12 17:37:07 -0300

    new file:   prepare/Learning02/Job.cs 	modified:   prepare/Learning02/Program.cs 	new file:   prepare/Learning02/Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job = new Job();
+        job._jobTitle = "Software Engineer";
+        job._company = "Microsoft";
+        job._startYear = 2010;
+        job._endYear = 2019;
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Architect";
+        job1._company = "Google";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Resume res = new Resume();
+        res._jobs.Add(job);
+        res._jobs.Add(job1);
+
     }
 }
