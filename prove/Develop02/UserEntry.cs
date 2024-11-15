0370a3ca0db2b9c73a75df22b1fb6320201d98f0
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-15 15:22:20 -0300

    modified:   prove/Develop02/UserEntry.cs

--- prove/Develop02/UserEntry.cs
+++ prove/Develop02/UserEntry.cs
@@ -17,14 +17,35 @@ public class UserEntry
 
     public int screenMenu()
     {
-        Console.WriteLine("Please select one of the following choices, typing the corresponding number.:");
-        Console.WriteLine("");
-        foreach (string m in menu)
-        {
-            Console.WriteLine($"{m}");
-        }
-        return 0;
+        bool exitMenu = false;
+        string userChoice;
+        int choice;
 
+        if (exitMenu == false)
+        {
+                
+            // Setting up and displaying the start menu.
+            Console.WriteLine("Please select one of the following choices, typing the corresponding number.:");
+            Console.WriteLine("");
+            foreach (string m in menu)
+            {
+                Console.WriteLine($"{m}");
+            }
+            Console.WriteLine("");
+
+            // Collecting user input, response.
+            userChoice = Console.ReadLine();
+
+            // Checking if user input is a number
+            if(int.TryParse(userChoice, out int number) == true)
+            {
+                choice = int.Parse(userChoice);
+
+
+            }
+            
+            return int.Parse();
+        }
     }
 
 
