7854caa2c5a6241572badff297fa307447db935b
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-15 10:01:08 -0300

    modified:   prove/Develop02/Program.cs

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -10,6 +10,12 @@ class Program
         q.setQuestion("What would you like to do?");
         q.setQuestion("WHow have you seen God's hand working in your life today?");
         q.setQuestion("What is the best, kindest thing you did today?");
+        q.setQuestion("Which scripture stood out to you the most today? Why?");
+        q.setQuestion("What has worried you most today? And how can you invite God to help you?");
+        q.setQuestion("What fact or action helped you get closer to your family?");
+        q.setQuestion("What did you do today that can increase your self-sufficiency?");
+        q.setQuestion("What interesting place have you been today?");
+        q.setQuestion("What happened today that helped your faith grow?");
 
         Console.WriteLine($"{q.getQuestion(1)}");
     }
