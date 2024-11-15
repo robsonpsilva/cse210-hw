9dcc7bd17120fa4efc0f67fa13a5305e7325212e
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-14 21:56:50 -0300

    modified:   prove/Develop02/Journal.cs 	modified:   prove/Develop02/QuestionList.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -6,18 +6,20 @@ class Journal
     //The journalRecordList variable is composed of a key that will have the format date-question 
     // and its value, which is the user's answer to the question.
     public Dictionary<string,string> _journalRecordList = new Dictionary<string, string>();
+    private QuestionList qlist = new QuestionList();
     
     public void getRecordList()
     {
         string[] part;
         string recordKeyData;
+        string question;
 
         foreach (KeyValuePair<string, string> input in _journalRecordList) 
         { 
             //The keys will be formatted as follows: data-questionNumber. 
             recordKeyData = input.Key; 
             part = recordKeyData.Split("-");
-
+            question = qlist.getQuestion(int.Parse(part[1]));
 
             Console.WriteLine($"Date: {part[0]} - {input.Value} anos"); 
         
--- prove/Develop02/QuestionList.cs
+++ prove/Develop02/QuestionList.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 public class QuestionList
 {
@@ -13,6 +14,36 @@ public class QuestionList
 
     }
 
+    public string getQuestion(int num)
+    {
+        return _questionRecords[num];
+
+    }
+
+    public void changeQuestion(int questionNumber, string question)
+    {
+        _questionRecords[questionNumber] = question;
+
+
+    }
+
+    public void deleteQuestion(int questionNumber)
+    {
+        _questionRecords.Remove(questionNumber);
+
+    }
+
+    public string getRandomQuestion()
+    {
+        Random random = new Random();
+        int min = 1;
+        int max = _questionRecords.Count;
+        int n;
+
+        n = random.Next(min, max);
+        return _questionRecords[n];
+
+    }
 
 }
 
