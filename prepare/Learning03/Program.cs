f2c05bbf21617e9b35115920859b4adc91c87880
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-18 09:11:18 -0300

    modified:   prepare/Learning03/Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -10,28 +10,28 @@ class Program
         Fraction f2 = new Fraction(6);
         Fraction f3 = new Fraction(6,7);
 
-        Console.WriteLine(f1.GetDecimalValue());
         Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
         Console.WriteLine("");
-        Console.WriteLine(f2.GetDecimalValue());
+
         Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine("");
-        Console.WriteLine(f3.GetDecimalValue());
-        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
         Console.WriteLine("");
 
-        f3.setBottonValue(4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine("");
+
         f3.setTopValue(3);
-
-        Console.WriteLine(f3.GetDecimalValue());
+        f3.setBottonValue(4);
         Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
         Console.WriteLine("");
 
-        f3.setBottonValue(3);
         f3.setTopValue(1);
-
-        Console.WriteLine(f3.GetDecimalValue());
+        f3.setBottonValue(3);
         Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
         Console.WriteLine("");
     }
 
