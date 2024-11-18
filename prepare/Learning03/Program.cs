c573f35cb0ad0be986e7d31515f54cc5d498e8a1
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-18 08:29:40 -0300

    new file:   prepare/Learning03/Fraction.cs 	modified:   prepare/Learning03/Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(6);
+        Fraction f3 = new Fraction(6,7);
     }
+
+    
 }
