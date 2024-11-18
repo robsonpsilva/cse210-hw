01179786b41dafa5a5de786a36363adee9bd9ff3
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-18 16:40:52 -0300

    modified:   prove/Develop03/Program.cs 	new file:   prove/Develop03/Reference.cs 	new file:   prove/Develop03/Scripture.cs 	new file:   prove/Develop03/Word.cs

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,22 @@
+
+class Scripture
+{
+    Reference reference = new Reference();
+
+    public void hideWords()
+    {
+
+    }
+
+    public string getScreenScripture()
+    {
+        return "";
+    }
+
+    public bool isFullHide()
+    {
+
+        return false;
+    }
+}
+
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,32 @@
+
+
+public class World
+{
+    bool displayStatus = true;
+    string word;
+
+    public void hideWord()
+    {
+        displayStatus = false;
+    }
+    
+    public void showWord()
+    {
+        displayStatus = true;
+    }
+
+    public bool isHidden()
+    {
+        return displayStatus;
+    }
+
+    public void setWord(string word)
+    {
+        this.word = word;
+    }
+
+    public string getWord()
+    {
+        return this.word;
+    }
+}
