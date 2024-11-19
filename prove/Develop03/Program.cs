7d1b0955a0ccb2e6d38429362da95a432defc600
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-19 17:57:28 -0300

    modified:   prove/Develop03/Program.cs 	modified:   prove/Develop03/Reference.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,5 +6,8 @@ class Program
     {
         Console.WriteLine("Hello Develop03 World!");
         Reference r = new Reference("teste", 33, 6, 6);
+        r.setVerse(6,"Nevertheless, I will bring health and healing to it; I will heal my people and will let them enjoy abundant peace and security.");
+        
+    
     }
 }
--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -28,14 +28,53 @@ class Reference
         this.endVerse = end; 
     }
 
-    public void setVerse(int verseNumber, string verseText)
+    public bool setVerse(int verseNumber, string verseText)
     {
-        string[] w = verseText.Split(" "); //Dividing the verse into its words so you can save them in a word list and manipulate them individually.
-        List<Word> verse = new List<Word>();
+        if (verseNumber>= startVerse && verseNumber <= endVerse){
+            if (verseText !=""){ //It only makes sense to create a verse if there is text in it.
+                string[] w = verseText.Split(" "); //Dividing the verse into its words so you can save them in a word list and manipulate them individually.
+                List<Word> verse = new List<Word>();
 
-        foreach (string word in w){
-            Word w1 = new Word(word); // Creating the Word object that will hold the words of the verse.
-            verse.Add(w1); //Adding the words one by one to the verses, which will allow them to be hidden individually afterwards.
+                foreach (string word in w)
+                {
+                    Word w1 = new Word(word); // Creating the Word object that will hold the words of the verse.
+                    verse.Add(w1); //Adding the words one by one to the verses, which will allow them to be hidden individually afterwards.
+                }
+
+                return true;
+            }
+            else{
+                return false; //It only makes sense to create a verse if there is text in it.
+            }
         }
+        else
+        {
+            /*
+                To ensure consistency, you need to make sure that the verse number 
+                is between the numbers of the initial and final verses.
+            */
+            return false; 
+        }   
     }
+
+    public string getHiddenReferenceString()
+    {
+        string output;
+        if (startVerse == endVerse)
+        {
+            output = $"{this.book} {this.chapter}:{this.startVerse} ";
+        }
+        else
+        {
+            output = $"{this.book} {this.chapter}:{this.startVerse}-{this.endVerse} ";
+        }
+        
+        for (int i = this.startVerse; i<= this.endVerse; i++)
+        {
+            output += $"{verses[i]} "; 
+        }
+
+        return output.Trim();
+    }
+
 }
