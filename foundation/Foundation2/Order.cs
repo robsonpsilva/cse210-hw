db0cd769590c3f24c9d1378f9f6baa83f930e015
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-29 20:03:30 -0300

    new file:   foundation/Foundation2/Order.cs 	modified:   foundation/Foundation2/Program.cs

---
+++ foundation/Foundation2/Order.cs
@@ -0,0 +1,65 @@
+
+class Order
+{
+    private Customer _customer;
+    private List<Product> _products = new List<Product>();
+
+    public Order(Customer customer)
+    {
+        this._customer = customer;
+    }
+
+    public void setProduct(Product product)
+    {
+        this._products.Add(product);
+    }
+
+    public string getPackingLabel()
+    {
+        string packingLabel = "";
+        int count = 0;
+        foreach(Product p in this._products)
+        {
+            count++;
+            if (count != this._products.Count())
+            {
+                packingLabel += $"Product Id: {p.getProductId()} - Product name: {p.getProductName()}" + Environment.NewLine;
+            }
+            else{
+                packingLabel += $"Product Id: {p.getProductId()} - Product name: {p.getProductName()}";
+            }
+            
+        }
+        return packingLabel;
+    }
+
+    public string getShippingLabel()
+    {
+        string shippingLabel =_customer.getCustomerName() + Environment.NewLine +
+        _customer.getCustomerAddress().getFullAddress();
+        return shippingLabel;
+    }
+
+    public double getTotalOrderCoast()
+    {
+        double total = 0;
+
+        //Please note that at this point we are calculating the total without considering whether the customer is in the USA or not.
+        foreach (Product p in this._products)
+        {
+            total += p.getTotalCost();
+        }
+        if(this._customer.liveInUSA())
+        {
+            //Adding the shipping price of $5 for shipments within the USA.
+            total += 5;
+        }
+        else
+        {
+           //Adding the shipping price of $35 for shipments outside the USA. 
+           total += 35; 
+        }
+
+        return total;
+    }
+}
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -7,15 +7,70 @@ class Program
     {
         Console.WriteLine("Hello Foundation2 World!");
 
+        //Loading customer address
         Address address = new Address("Rua Silva Teles, 30, Andaraí", "Rio de Janeiro","RJ", "Brazil");
         
+        //Loading customer data
         Customer customer = new Customer("Robson Paulo da Silva", address);
 
-        Product product = new Product(1, "Logitech Mouse", 5.60 , 2);
+        //Once the customer for the order has been defined, the next step is to create a purchase order.
+        Order order = new Order(customer);
+
+        //Then the products chosen by the customer need to be included in their purchase order.
+        order.setProduct(new Product(1, "Logitech Mouse", 5.60 , 2));
+        order.setProduct(new Product(2, "Keyboard Dell", 10.0 , 1));
+        order.setProduct(new Product(3, "Laptop Samsung", 500.0 , 1));
+        order.setProduct(new Product(4, "Mouse Pad RedDragon", 2.0 , 1));
+
 
         Console.Clear();
-        Console.WriteLine(customer.getCustomerName());
-        Console.WriteLine(customer.getCustomerAddress().getFullAddress());
-        Console.WriteLine(product.getTotalCost());
+
+        //Once the order is completed, we will inform you of the total value of the order.       
+        
+        
+        Console.WriteLine("First Client");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("Shipping label");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine(order.getShippingLabel());
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("Packing label");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine(order.getPackingLabel());
+        
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine($"Order total including shipping: ${order.getTotalOrderCoast()}");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        //Console.WriteLine(product.getTotalCost());
+
+        //---------------------------------------------------------------------
+        //This second part refers to the second customer and his order.
+        //Loading other customer address
+        address = new Address("2520 21st Street Apt 720", "Springfield","VT", "USA");
+        
+        //Loading customer data
+        customer = new Customer("John Smith", address);
+
+        //Once the customer for the order has been defined, the next step is to create a purchase order.
+        order = new Order(customer);
+
+        order.setProduct(new Product(1, "Set of frying pans", 42.99 , 1));
+        order.setProduct(new Product(2, "Libbey Ascent 16-Piece Tumbler and Rocks Glass Set", 10.0 , 2));
+        order.setProduct(new Product(3, "Riveira Bamboo Wooden Spoons for Cooking 6-Piece", 500.0 , 1));
+
+        Console.WriteLine("");
+        Console.WriteLine("Second client");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("Shipping label");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine(order.getShippingLabel());
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine("Packing label");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine(order.getPackingLabel());
+        Console.WriteLine("---------------------------------------------------------------------------");
+        Console.WriteLine($"Order total including shipping: ${order.getTotalOrderCoast()}");
+        Console.WriteLine("---------------------------------------------------------------------------");
+        
     }
 }
