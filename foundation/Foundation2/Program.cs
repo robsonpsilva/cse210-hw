591fac2dcef6c664936c322a9fc9a16fc72663a6
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-11-29 21:23:00 -0300

    modified:   foundation/Foundation2/Program.cs

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -17,7 +17,6 @@ class Program
         Order order = new Order(customer, new Product(1, "Logitech Mouse", 5.60 , 2));
 
         //Then the products chosen by the customer need to be included in their purchase order.
-        //order.setProduct(new Product(1, "Logitech Mouse", 5.60 , 2));
         order.setProduct(new Product(2, "Keyboard Dell", 10.0 , 1));
         order.setProduct(new Product(3, "Laptop Samsung", 500.0 , 1));
         order.setProduct(new Product(4, "Mouse Pad RedDragon", 2.0 , 1));
@@ -53,8 +52,6 @@ class Program
 
         //Once the customer for the order has been defined, the next step is to create a purchase order.
         order = new Order(customer, new Product(1, "Set of frying pans", 42.99 , 1));
-
-        //order.setProduct(new Product(1, "Set of frying pans", 42.99 , 1));
         order.setProduct(new Product(2, "Libbey Ascent 16-Piece Tumbler and Rocks Glass Set", 10.0 , 2));
         order.setProduct(new Product(3, "Riveira Bamboo Wooden Spoons for Cooking 6-Piece", 500.0 , 1));
 
