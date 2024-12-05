dfab6039518525b48b59d41093c04ccb88acb627
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-12-05 16:02:45 -0300

    new file:   prove/Develop05/BreathingActivity.cs

---
+++ prove/Develop05/BreathingActivity.cs
@@ -0,0 +1,12 @@
+
+using System.Diagnostics;
+
+public class BreathingActivity : Activity
+{
+    public BreathingActivity(string activityName, string activityDescription, int activityDuration):
+    base(activityName,activityDescription,activityDuration)
+    {
+        
+    }
+
+}
