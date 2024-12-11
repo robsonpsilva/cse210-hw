42f5a0db0a08b23727ce439590ebbc6375cec6c0
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-12-11 16:51:22 -0300

    modified:   prove/Develop06/Goal.cs

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -1,7 +1,16 @@
 
+using System.Runtime.CompilerServices;
+
 public class Goal{
-    public Goal()
+    private int _goalAccomplishedPoints = 0;
+    private string _name;
+    private string _description;
+    private int amountOfPoints = 0;
+    public Goal(string name, string description, int goalAccomplishedPoints)
     {
+        this._name = name;
+        this._description = description;
+        this._goalAccomplishedPoints = goalAccomplishedPoints;
         
     }
 }
