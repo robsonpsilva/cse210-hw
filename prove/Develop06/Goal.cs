e2c97a74b6d5781abce68ec223d08c5e1486402a
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2024-12-12 06:44:17 -0300

    modified:   prove/Develop06/Goal.cs

--- prove/Develop06/Goal.cs
+++ prove/Develop06/Goal.cs
@@ -1,11 +1,10 @@
 
 using System.Runtime.CompilerServices;
 
-public class Goal{
+public abstract class Goal{
     private int _goalAccomplishedPoints = 0;
     private string _name;
     private string _description;
-    private int amountOfPoints = 0;
     public Goal(string name, string description, int goalAccomplishedPoints)
     {
         this._name = name;
@@ -13,4 +12,14 @@ public class Goal{
         this._goalAccomplishedPoints = goalAccomplishedPoints;
         
     }
+
+    
+    public abstract void RecordEvent();
+    public abstract bool IsComplete();
+    public abstract string getStringRepresantation();
+    public virtual string getDetailsString()
+    {
+        
+         return $"[] {this._name} ({this._description})";
+
 }
